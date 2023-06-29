2f06c493f62dc9b2e94a89fb0436e6cf9def54ea
Carolina <carolinabuni@gmail.com>
2023-06-29 12:30:41 +0200

    add juego con window-event

--- 3-window-events/main.js
+++ 3-window-events/main.js
@@ -1,6 +1,39 @@
+const OnCompleteGame = () => {
+    alert('Juego completado! 🎊')
+};
+
+const buttonElement = document.querySelector('#complete-game')
+buttonElement.addEventListener('click', OnCompleteGame);
+
+const pTextList = [];
+
 const onMouseMove = (event) => {
-    console.log(event);
-}
+    if(event.target.tagName === 'P'){
+        if(!pTextList.includes(event.target.className)){
+        pTextList.push(event.target.className);
+            if(pTextList.length === 5){
+                document.body.style.height = '200vh';
+            }
+        }
+    }
+};
+
+let enteredPassword = false;
+
+const onScroll = () => {
+    if(!enteredPassword && window.scrollY > window.outerHeight / 2){
+        const password = prompt('Introduce la contraseña:');
+
+        if(password === '1234'){
+            buttonElement.style.display = 'block';
+            enteredPassword = true;
+        }
+    }
+};
 
 window.addEventListener('mousemove',onMouseMove);
+window.addEventListener('scroll',onScroll);
+
+
+
 
