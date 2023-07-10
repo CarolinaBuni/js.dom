f6341784511d44330c89a6b5161c159b16aeb677
Carolina <carolinabuni@gmail.com>
2023-07-10 16:40:07 +0200

    ejercicio 1

--- Ejercicios-dom/01/main.js
+++ Ejercicios-dom/01/main.js
@@ -10,21 +10,24 @@ console.log('Este es el H1:', h1Element.textContent);
 
 //* 1.3 Usa querySelector para mostrar por consola todos los p
 
-const pElement = document.querySelectorAll('p');
-console.log('Estos son todos los p:', pElement.textContent);
+const elementosP = document.querySelectorAll('p');
+elementosP.forEach(elemento => console.log(elemento.textContent));
+
 
 //* 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
 
-const pokemonElements = document.querySelectorAll('.pokemon');
-console.log('Estos son:', pokemonElements);
+const elementosPokemon = document.querySelectorAll('.pokemon');
+elementosPokemon.forEach(elemento => console.log(elemento.textContent));
+
 
 //* 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo data-function="testMe".
 
-const TestMeElement = document.querySelectorAll('[data-function="testMe"]');
-console.log('Atributos TestMe:', TestMeElement);
+const elementosTestMe = document.querySelectorAll('[data-function="testMe"]');
+elementosTestMe.forEach(elemento => console.log(elemento.textContent));
+
 
 //* 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo data-function="testMe".
 
 const thirdAtribute = document.querySelector('[data-function="testMe"]:nth-of-type(3)');
-console.log('Tercer personaje:',thirdAtribute);
+console.log('Tercer personaje:',thirdAtribute.textContent);
 
